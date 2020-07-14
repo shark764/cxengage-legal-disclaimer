bff47b168abd84ff9513a23962952109664668c1
Farid Hernandez <farid.hdz.64@gmail.com>
2020-07-14 07:02:02 -0600

    Removing translated message from error component since it cannot accept params.

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,12 +1,5 @@
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
 
-import messages from './messages';
-
-const NotFound = () => (
-  <h1>
-    <FormattedMessage {...messages.notFound} />
-  </h1>
-);
+const NotFound = () => <h1>Not Found</h1>;
 
 export default NotFound;
